32cd4420f9f6642211d39ccf02d3cdd03d966947
Valentino Giardino <valentinogiardino17@gmail.com>
2024-03-10 18:47:24 -0300

    add image generation

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "oaidalleapiprodscus.blob.core.windows.net",
+        },
+  
+        {
+          protocol: "http",
+          hostname: "oaidalleapiprodscus.blob.core.windows.net",
+        },
+      ],
+    },
+  };
+  
+  export default nextConfig;
+  
