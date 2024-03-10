3527709eeb6edb54789ade2e931301fe0a7e3fe7
Valentino Giardino <valentinogiardino17@gmail.com>
2024-03-10 20:11:18 -0300

    implement api limits

--- app/api/video/route.ts
+++ app/api/video/route.ts
@@ -1,6 +1,7 @@
 import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 import Replicate from 'replicate';
+import { incrementApiLimit, checkApiLimit } from "@/lib/api-limit";
 
 const replicate = new Replicate({
     auth: process.env.REPLICATE_API_TOKEN!,
@@ -21,7 +22,11 @@ export async function POST(
     if (!prompt) {
       return new NextResponse("Prompt is required", { status: 400 });
     }
+    const freeTrial = await checkApiLimit();
 
+    if(!freeTrial){
+      return new NextResponse("Free trial has expired.", { status: 403 });
+    }
     const response = await replicate.run(
       "anotherjesse/zeroscope-v2-xl:9f747673945c62801b13b84701c783929c0ee784e4748ec062204894dda1a351",
       {
@@ -30,6 +35,7 @@ export async function POST(
         }
       }
     );
+    await incrementApiLimit();
 
 
     return NextResponse.json(response);
